0e391efb2e91a7e94eaca0be23e422977f27fce5
Madeline Cameron <maddie@qnzl.co>
2021-02-28 11:32:52 -0500

    Clean up

--- gen-key.js
+++ gen-key.js
@@ -2,7 +2,7 @@ const { writeFileSync } = require(`fs`)
 const { JWK } = require(`jose`)
 
 ;(async () => {
-  const key = await JWK.generate(`RSA`)
+  const key = await JWK.generate(`EC`)
 
   const publicKey = key.toPEM()
   const privateKey = key.toPEM({ passphrase: process.env.PEM_FILE_PASSWORD })
